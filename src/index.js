2a55c725ffae3b4364eb555afba1e959444419f3
dabrown <dabrown@goscoutgo.com>
2017-08-04 13:23:51 -0600

    -

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function formatName(user) {
+  return user.firstName + ' ' + user.lastName;
+}
+
+const user = {
+  firstName: 'Harper',
+  lastName: 'Perez'
+};
+
+
+
+ReactDOM.render(
+  element,
+  document.getElementById('root')
+);
