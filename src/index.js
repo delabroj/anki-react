6857b90375f53208080fa08f968aab7a6d8594f0
dabrown <dabrown@goscoutgo.com>
2017-08-07 09:14:18 -0600

    -

--- src/index.js
+++ src/index.js
@@ -1,23 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class Toggle extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {isToggleOn: true};
 
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-
-
-  render() {
-    return (
-      <button onClick={this.handleClick}>
-        {this.state.isToggleOn ? 'ON' : 'OFF'}
-      </button>
-    );
-  }
-}
 
 ReactDOM.render(<Toggle />, document.getElementById('root'));
