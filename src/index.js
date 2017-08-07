9b02f9f93f3b06dce13f37f817aa8cc2bb72aa62
dabrown <dabrown@goscoutgo.com>
2017-08-07 08:20:42 -0600

    -

--- src/index.js
+++ src/index.js
@@ -1,11 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-function formatName(user) {
-  return user.firstName + ' ' + user.lastName;
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {date: new Date()};
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+      </div>
+    );
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({
+      date: new Date()
+    });
+  }
 }
 
-const user = {
-  firstName: 'Harper',
-  lastName: 'Perez'
-};
+ReactDOM.render(<Clock />, document.getElementById('root'));
--- src/index.solution.js
+++ src/index.solution.js
@@ -1,22 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-function formatName(user) {
-  return user.firstName + ' ' + user.lastName;
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {date: new Date()};
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+      </div>
+    );
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({
+      date: new Date()
+    });
+  }
 }
 
-const user = {
-  firstName: 'Harper',
-  lastName: 'Perez'
-};
-
-const element = (
-  <h1>
-    Hello, {formatName(user)}!
-  </h1>
-);
-
-ReactDOM.render(
-  element,
-  document.getElementById('root')
-);
+ReactDOM.render(<Clock />, document.getElementById('root'));
