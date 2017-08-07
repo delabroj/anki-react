ed7f68faa3828a70dcbb5b09840f56acb03bfc1f
dabrown <dabrown@goscoutgo.com>
2017-08-07 13:26:27 -0600

    -

--- src/index.js
+++ src/index.js
@@ -1,17 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-function ActionLink() {
-  function handleClick(e) {
-    e.preventDefault();
-    console.log('The link was clicked.');
-  }
 
-  return (
-    <a href="#" onClick={handleClick}>
-      Click me
-    </a>
-  );
-}
 
-ReactDOM.render(<ActionLink />, document.getElementById('root'));
+ReactDOM.render(<Clock />, document.getElementById('root'));
