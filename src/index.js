42b3951b1d998b98fec5bbe7963fee8172e97c46
dabrown <dabrown@goscoutgo.com>
2017-08-07 08:47:56 -0600

    -

--- src/index.js
+++ src/index.js
@@ -1,37 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class Clock extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {date: new Date()};
+function ActionLink() {
+  function handleClick(e) {
+    e.preventDefault();
+    console.log('The link was clicked.');
   }
 
-  render() {
-    return (
-      <div>
-        <h1>Hello, world!</h1>
-        
-      </div>
-    );
-  }
-
-  componentDidMount() {
-    this.timerID = setInterval(
-      () => this.tick(),
-      1000
-    );
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.timerID);
-  }
-
-  tick() {
-    this.setState({
-      date: new Date()
-    });
-  }
+  return (
+    <a href="#" onClick={handleClick}>
+      Click me
+    </a>
+  );
 }
 
-ReactDOM.render(<Clock />, document.getElementById('root'));
+ReactDOM.render(<ActionLink />, document.getElementById('root'));
--- src/index.solution.js
+++ src/index.solution.js
@@ -9,10 +9,7 @@ class Clock extends React.Component {
 
   render() {
     return (
-      <div>
-        <h1>Hello, world!</h1>
-        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
-      </div>
+      <h2>{this.state.date.toLocaleTimeString()}</h2>
     );
   }
 
