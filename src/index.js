07bcdb68bd0d133add51e905438dcf311914d4a8
dabrown <none>
2017-08-15 14:35:40 -0600

    -

--- src/index.js
+++ src/index.js
@@ -14,6 +14,9 @@ class App extends Component {
               <Route path='instagram' component={Instagram} />
             </Route>
             <Route path='third' component={Third} />
+            <Route path='namedcomponents' component={NamedComponents}>
+              <IndexRoute components={{title: Title, subTitle: SubTitle}} />
+            </Route>
             <Route path='*' component={NotFound} />
           </Route>
         </Router>
@@ -22,6 +25,16 @@ class App extends Component {
     )
   }
 }
+
+const Title = () => (
+  <h1>Hello from Title Component</h1>
+)
+
+const SubTitle = () => (
+  <h1>Hello from SubTitle Component</h1>
+)
+
+
 
 const Address = (props) => (
   <div>
@@ -52,6 +65,7 @@ const Navigate = () => (
     <Link to="/">Home</Link>&nbsp;
     <Link to="/address">Address</Link>&nbsp;
     <Link to="/third">Third</Link>&nbsp;
+    <Link to="/namedcomponents">Named Components</Link>&nbsp;
   </div>
 );
 
